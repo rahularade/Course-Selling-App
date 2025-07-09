fc622894a5371d9285debf32d76d512b2c48f869
rahularade <araderahul9196@gmail.com>
2025-07-09 16:12:58 +0530

    Add dotenv package

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const { userRouter } = require("./routes/user.routes");
 const { courseRouter } = require("./routes/course.routes");
 const { creatorRouter } = require("./routes/creator.routes");
 
+dotenv.config();
 const app = express();
 app.use(express.json())
-
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/creator", creatorRouter);
 app.use("api/v1/course", courseRouter);
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
